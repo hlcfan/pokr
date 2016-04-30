50e7ce6bbb444938c26a8bba1aa2b37758ff71b2
Alex S <hlcfan.yan@gmail.com>
2016-04-30 17:43:55 +0800

    Fix variable name in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ user_room1 = UserRoom.create user_id: 1, room_id: 1
 user_room2 = UserRoom.create user_id: 2, room_id: 1
 
 story1 = Story.create room_id: 1, link: 'http://baidu.com', desc: 'his file should contain all the record creation needed to seed the database with its default values.'
-story1 = Story.create room_id: 1, link: 'http://google.com', desc: 'The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).'
+story2 = Story.create room_id: 1, link: 'http://google.com', desc: 'The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).'
 
 user_room_story1 = UserStoryPoint.create user_id: 1, story_id: 1, points: 8
 user_room_story2 = UserStoryPoint.create user_id: 2, story_id: 1, points: 13
