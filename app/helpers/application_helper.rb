e7d7442571fe74161190347cbbcb1bf61f3d485c
Alex S <hlcfan.yan@gmail.com>
2016-05-15 17:42:51 +0800

    Update flash messages style with Bootstrap alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
     Net::HTTP.post_form(uri, :message => envelope.to_json)
   end
 
+  def flash_messages opts = {}
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
+              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+              concat message 
+            end)
+    end
+    nil
+  end
+
+  private
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
