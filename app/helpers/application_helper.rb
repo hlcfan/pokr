79d4eabc694780cde9674f99628d6697c2b06f1e
Alex S <hlcfan.yan@gmail.com>
2016-06-26 11:11:39 +0800

    Move methods to RoomsHelper where they belongs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def broadcast(channel, message)
-    faye_client.publish channel, message
-  end
-
   def flash_messages opts = {}
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
@@ -18,10 +14,6 @@ module ApplicationHelper
 
   def bootstrap_class_for flash_type
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
-  end
-
-  def faye_client
-    @faye_client ||= Faye::Client.new(Rails.configuration.faye_server)
   end
 
 end
