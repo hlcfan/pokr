5c246fdce2fff29270422f83506f0eb5c1543802
Alex S <hlcfan.yan@gmail.com>
2016-06-27 15:28:08 +0800

    Add helpers to set title, description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 module ApplicationHelper
 
+  def title(title)
+    content_for(:title, title)
+  end
+
+  def description description
+    description = truncate description, length: 160, seperator: ' '
+    content_for :description_meta_content, description
+  end
+
+  def default_description
+    "Pokr, is an efficiency pointing poker or planning poker aim for improving the efficiency and user experience of agile teams"
+  end
+
   def flash_messages opts = {}
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
