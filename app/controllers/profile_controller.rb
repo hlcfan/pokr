320ea3f494694c464e9916ad9b3ceffcc59fae8a
Alex S <hlcfan.yan@gmail.com>
2016-06-29 21:46:05 +0800

    Therefore profile can be udpated

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,8 +1,10 @@
 class ProfileController < ApplicationController
 
+  before_action :authenticate_user!
+  before_action :set_user
 
   def show
-    
+
   end
 
   def edit
@@ -10,7 +12,21 @@ class ProfileController < ApplicationController
   end
 
   def update
-    
+    if @user.update_attributes profile_params
+      redirect_to profile_path, flash: { success: 'Your profile updated successfully' }
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def profile_params
+    params.require(:user).permit(:name)
   end
 
 end
