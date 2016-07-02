94a2625bff97bd734453c1dcde63cc330f25c41c
Alex S <hlcfan.yan@gmail.com>
2016-07-02 18:08:03 +0800

    Get user rooms

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
 
   def index
-    @created_rooms = 
+    @created_rooms = current_user.created_rooms
+    @partipated_rooms = current_user.rooms
   end
 
 end
