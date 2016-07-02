b4ad3af2212d3ba43c73f23b159c6fb790f75fe0
Alex S <hlcfan.yan@gmail.com>
2016-07-02 17:56:50 +0800

    Add created_by to rooms table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627085919) do
+ActiveRecord::Schema.define(version: 20160702092848) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20160627085919) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "slug",       limit: 255, null: false
+    t.integer  "created_by", limit: 4
   end
 
+  add_index "rooms", ["created_by"], name: "index_rooms_on_created_by", using: :btree
   add_index "rooms", ["name"], name: "index_rooms_on_name", using: :btree
   add_index "rooms", ["slug"], name: "index_rooms_on_slug", unique: true, using: :btree
 
