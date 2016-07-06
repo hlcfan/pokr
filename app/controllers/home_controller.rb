9fd90be2a5c763b6abaadafc35686bdcd617af44
Alex S <hlcfan.yan@gmail.com>
2016-07-06 19:43:14 +0800

    Redirect to dash board if user logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
 
   def index
+    redirect_to dashboard_index_path and return if current_user
     render layout: false
   end
 
   def director
-    room = Room.find_by(name: params[:name])
+    room = Room.find_by(slug: params[:name])
     if room.present?
       redirect_to room_path(room)
     else
