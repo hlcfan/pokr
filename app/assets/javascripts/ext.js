f9eebb1112c1da6d7677c134665098cf0ab6b905
Alex S <hlcfan.yan@gmail.com>
2016-07-10 14:01:54 +0800

    Extension for Javascript Array to clean empty element

---
+++ app/assets/javascripts/ext.js
@@ -0,0 +1,9 @@
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {         
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
