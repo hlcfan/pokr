71a36cd9625d92b6ea19d8e80373a49e044b2368
Alex S <hlcfan.yan@gmail.com>
2016-07-17 15:42:05 +0800

    Add before action for dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @created_rooms = current_user.created_rooms
     @partipated_rooms = current_user.rooms
