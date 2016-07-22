0a1ce1a625695477950a0dd566ca012121eed6e1
Alex S <hlcfan.yan@gmail.com>
2016-07-22 10:55:21 +0800

    Update ENV vars name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['mailer_user'],
-    :password => ENV['mailer_password'],
+    :user_name => ENV['MAILER_USERNAME'],
+    :password => ENV['MAILER_PASSWORD'],
     :domain => 'pokrex.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
