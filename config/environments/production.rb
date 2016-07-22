79542109009ec07d8c913625f023c70534aa0d10
Alex S <hlcfan.yan@gmail.com>
2016-07-22 11:29:50 +0800

    Specify smtp host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.action_mailer.default_url_options = { :host => 'pokrex.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
