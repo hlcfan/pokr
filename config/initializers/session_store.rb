7a22fd57c63480799f02f54ef7470f73b1182e2b
Alex S <hlcfan.yan@gmail.com>
2016-07-25 08:58:18 +0800

    This is what after ran rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_poker_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_poker_session'
