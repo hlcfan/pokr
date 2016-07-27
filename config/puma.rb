d48cb191132ef3ec2a0b87f5ff8b0cfa08cb72cf
Alex S <hlcfan.yan@gmail.com>
2016-07-27 09:37:13 +0800

    Shit, havent update Turbolinks event

--- config/puma.rb
+++ config/puma.rb
@@ -14,10 +14,6 @@ workers 4
 threads 8, 16
 preload_app!
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
