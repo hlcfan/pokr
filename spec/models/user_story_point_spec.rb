96353fc2f6c3424aaafb0ffcf1476e8cdd04704a
Alex S <hlcfan.yan@gmail.com>
2016-08-01 13:08:41 +0800

    Add'em all

--- spec/models/user_story_point_spec.rb
+++ spec/models/user_story_point_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UserStoryPoint, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".vote" do
+    let(:user_id) { 1 }
+    let(:story_id) { 1 }
+    it "creates record when user votes first time" do
+      UserStoryPoint.vote user_id, story_id, 13
+      expect(UserStoryPoint.find_by(user_id: user_id, story_id: story_id).points).to eq "13"
+    end
+
+    it "updates record when user re-vote" do
+      UserStoryPoint.vote user_id, story_id, 20
+      expect(UserStoryPoint.find_by(user_id: user_id, story_id: story_id).points).to eq "20"
+    end
+  end
 end
