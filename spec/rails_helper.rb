d33bb1a9c7be02535153cae95df94b97c79b877b
Alex S <hlcfan.yan@gmail.com>
2016-08-07 10:50:17 +0800

    Setup simple cov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'devise'
 require 'support/controller_macros'
+require 'simplecov'
+
+SimpleCov.start
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
