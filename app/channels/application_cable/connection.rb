0cb6fb8b3fa8bc68151e95f14da41ad6abd21616
Alex S <hlcfan.yan@gmail.com>
2016-08-24 15:07:14 +0800

    Setup room id for streaming

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # uniquely identify this connection
+    identified_by :current_room_id
+
+    # called when the client first connects
+    def connect
+      self.current_room_id = find_room_id
+    end
+
+    protected
+
+    def find_room_id
+      # session isn't accessible here
+      if room_id = cookies[:room_id]
+        room_id
+      else
+        # writes a log and raises an exception
+        reject_unauthorized_connection
+      end
+    end
   end
 end
