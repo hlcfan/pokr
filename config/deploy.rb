09f4ef0c0b3bd11612fff32bcd6d528e2bfe13c9
Alex S <hlcfan.yan@gmail.com>
2016-08-24 15:53:40 +0800

    Add more shared paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :rails_env, 'production'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['config/database.yml', 'log', 'tmp/pids', 'tmp/sockets']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
