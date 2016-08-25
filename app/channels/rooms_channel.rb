e16b9f945128ecd25d76f4e7745d98ab11e681ac
Alex S <hlcfan.yan@gmail.com>
2016-08-25 23:14:28 +0800

    Implement set_story_point

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -12,9 +12,7 @@ class RoomsChannel < ApplicationCable::Channel
 
   def vote data
     payload = data["data"]
-    room_id = data["roomId"]
-    # TODO: find from cache
-    @room = Room.find_by slug: room_id
+    set_room data["roomId"]
     if valid_vote? payload
       UserStoryPoint.vote(current_user.id,
                       payload["story_id"],
@@ -28,7 +26,32 @@ class RoomsChannel < ApplicationCable::Channel
     end
   end
 
+  def set_story_point
+    payload = data["data"]
+    set_room data["roomId"]
+
+    user_room = UserRoom.find_by_with_cache(user_id: current_user.id, room_id: @room.id)
+
+    if user_room.moderator? && @room.valid_vote_point?(payload["point"])
+      story = Story.find_by id: payload["story_id"], room_id: @room.id
+      if story
+        story.update_attribute :point, payload["point"]
+        @room.update_attribute :status, nil
+        broadcaster "rooms/#{@room.slug}",
+                    type: "action",
+                    person_id: user_story_point.user_id,
+                    story_id: user_story_point.story_id,
+                    points: user_story_point.points
+      end
+    end
+  end
+
   private
+
+  def set_room room_id
+    # TODO: find from cache
+    @room = Room.find_by slug: room_id
+  end
 
   def valid_vote? payload
     @room.valid_vote_point?(payload["points"]) && payload["story_id"].present?
