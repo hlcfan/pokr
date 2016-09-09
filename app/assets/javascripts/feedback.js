218f8afb8477cddabaa3c69c1fa0a75a237567f6
Alex S <hlcfan.yan@gmail.com>
2016-09-09 15:01:59 +0800

    Solid modal

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function(event) {
   $(".feedback-holder").on("click", function() {
-    $("#feedback .modal").modal();
+    $("#feedback .modal").modal({keyboard: false, backdrop: 'static'});
   });
 });
