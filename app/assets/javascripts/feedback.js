aae35fde4740e06dd8725b1734f68ce8a681a9df
Alex S <hlcfan.yan@gmail.com>
2016-09-09 15:44:57 +0800

    Add interactions to feedback form

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -1,5 +1,25 @@
 $(document).on('turbolinks:load', function(event) {
   $(".feedback-holder").on("click", function() {
+    $("#feedback #message").val("");
+    $("#feedback .new-form").show();
+    $("#feedback .submitted-form").hide();
     $("#feedback .modal").modal({keyboard: false, backdrop: 'static'});
   });
+
+  $("#feedback .submit").on("click", function(e) {
+    var formData = {
+      'email'    : $('#feedback input[name=email]').val(),
+      'feedback' : $('#feedback #message').val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: "/home/feedback",
+      data: formData
+    }).done(function(data) {
+      $("#feedback .new-form").hide();
+      $("#feedback .submitted-form").show();
+    });
+  });
 });
+
