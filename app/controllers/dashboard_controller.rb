c599084bc720a1e6fdeb3fabfc2ee3c34b279e5f
Alex S <hlcfan.yan@gmail.com>
2016-09-10 14:41:03 +0800

    Sort by created_at DESC

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,8 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @created_rooms = current_user.created_rooms
-    @partipated_rooms = current_user.rooms
+    @created_rooms = current_user.created_rooms.order("created_at DESC")
+    @partipated_rooms = current_user.rooms.order("created_at DESC")
   end
 
 end
