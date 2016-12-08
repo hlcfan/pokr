53d9dd11a8b644a8c5e3d1296c1c334dc1a8d63f
Alex S <hlcfan.yan@gmail.com>
2016-12-08 13:28:58 +0800

    Code format

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -7,29 +7,28 @@ $(document).on('turbolinks:load', function(event) {
     var data = {
       labels : chartLabels,
       datasets : [{
-          label: "Story",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: chartData,
-          spanGaps: false,
-          borderWidth: 3
-        }
-      ]
+        label: "Story",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: chartData,
+        spanGaps: false,
+        borderWidth: 3
+      }]
     }
 
     var myNewChart = new Chart(ctx, {
