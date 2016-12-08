7b6230f27db435c9832a1e73ce5addc95cc90f6a
Alex S <hlcfan.yan@gmail.com>
2016-12-08 12:15:34 +0800

    Draw chart when sufficient data

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,46 +1,47 @@
 $(document).on('turbolinks:load', function(event) {
-  var ctx = document.getElementById("myChart");
+  if (typeof chartLabels !== "undefined") {
+    var ctx = document.getElementById("myChart");
 
-  var data = {
-    labels : chartLabels,
-    datasets : [{
-            label: '# of Votes',
-            data: chartData,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }
-    ]
-  }
-
-  var myNewChart = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        yAxes: [{
-          ticks: {
-              beginAtZero:true
-          }
-        }]
-      }
+    var data = {
+      labels : chartLabels,
+      datasets : [{
+              label: '# of Votes',
+              data: chartData,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+            }
+      ]
     }
-  });
 
+    var myNewChart = new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
+  }
 });
