de280369c4991b75afc559927d5a1bcc363d6703
Alex S <hlcfan.yan@gmail.com>
2016-12-08 14:41:54 +0800

    Output groomed stories

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,8 @@ class DashboardController < ApplicationController
   def index
     @created_rooms = current_user.created_rooms.order("created_at DESC")
     @partipated_rooms = current_user.rooms.order("created_at DESC")
-    
+    room_ids = @partipated_rooms.where(status: Room::DRAW).map(&:id)
+    @stories = Story.where(room_id: room_ids).limit(10)
   end
 
 end
