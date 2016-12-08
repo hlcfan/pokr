aeccc98081965c9b552744c881e06df9f295e601
Alex S <hlcfan.yan@gmail.com>
2016-12-08 13:27:55 +0800

    Initialize tooltip & prepare chart data

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,35 +1,39 @@
 $(document).on('turbolinks:load', function(event) {
+  $('[data-toggle="tooltip"]').tooltip()
+
   if (typeof chartLabels !== "undefined") {
     var ctx = document.getElementById("myChart");
 
     var data = {
       labels : chartLabels,
       datasets : [{
-              label: '# of Votes',
-              data: chartData,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }
+          label: "Story",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: chartData,
+          spanGaps: false,
+          borderWidth: 3
+        }
       ]
     }
 
     var myNewChart = new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: data,
       options: {
         responsive: true,
