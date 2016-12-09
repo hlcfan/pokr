c8d03910bfc06e183948ce4ac793ce4f9f3a4818
Alex S <hlcfan.yan@gmail.com>
2016-12-09 21:35:52 +0800

    Refactor: remove extra instance variables

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,6 @@ class DashboardController < ApplicationController
 
   def index
     @user = UserPresenter.new(current_user)
-    @created_rooms = @user.created_rooms
-    @partipated_rooms = @user.participated_room
-    room_ids = @user.recent_rooms.map(&:id)
-    @stories = Story.where(room_id: room_ids).limit(10)
   end
 
 end
--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -7,31 +7,38 @@ class UserPresenter
   end
 
   def created_rooms
-    participated_room.select do |room|
+    participated_rooms.select do |room|
       user.id == room.created_by
     end
   end
 
-  def participated_room
-    @participated_room ||= user.rooms.order("created_at DESC").to_a
+  def participated_rooms
+    @participated_rooms ||= user.rooms.order("created_at DESC").to_a
   end
 
   def recent_rooms
-    participated_room.take(3).select do |room|
-      room.status == Room::DRAW
+    @recent_rooms ||= begin
+      participated_rooms.take(3).select do |room|
+        room.status == Room::DRAW
+      end
     end
   end
 
   def timestamp_for_line_chart
-    participated_room.reverse.map do |room|
+    participated_rooms.reverse.map do |room|
       room.created_at.strftime("%b %d")
     end
   end
 
   def story_size_for_line_chart
-    participated_room.reverse.map do |room|
+    participated_rooms.reverse.map do |room|
       room.stories.size
     end
+  end
+
+  def recent_stories
+    room_ids = recent_rooms.map(&:id)
+    Story.where(room_id: room_ids).limit(10)
   end
 
 end
