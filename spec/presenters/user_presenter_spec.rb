117d02993688b7a49111a0d245483a5ee00fa721
Alex S <hlcfan.yan@gmail.com>
2016-12-10 18:22:00 +0800

    Add tests for new method and modify tests for method change

--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -29,11 +29,14 @@ RSpec.describe UserPresenter do
   end
 
   describe "#recent_stories" do
-    it "returns recent groomed stories" do
+    it "returns recent groomed stories by current user" do
       story_1 = Story.create(link: 'http://jira.com/123', point: 1)
       sleep 1
       story_2 = Story.create(link: 'http://jira.com/345', point: 3)
       story_3 = Story.create(link: 'http://jira.com/567')
+      UserStoryPoint.create(user_id: user.id, story_id: story_1.id, points: 1)
+      UserStoryPoint.create(user_id: user.id, story_id: story_2.id, points: 3)
+      UserStoryPoint.create(user_id: user.id, story_id: story_3.id, points: 8)
 
       expect(user_presented.recent_stories).to eq [story_2, story_1]
     end
@@ -82,6 +85,36 @@ RSpec.describe UserPresenter do
       UserStoryPoint.create(user_id: user.id, story_id: story_4.id, points: 13)
 
       expect(user_presented.time_spent).to eq 2.0
+    end
+  end
+
+  describe "#skip_rate" do
+    let!(:story_1) { Story.create(link: "link_1", room_id: 1, point: 3) }
+    let!(:story_2) { Story.create(link: "link_2", room_id: 1, point: "null") }
+
+    it "returns skip rate if user voted any stories" do
+      UserStoryPoint.create(user_id: user.id, story_id: story_1.id, points: 1)
+      UserStoryPoint.create(user_id: user.id, story_id: story_2.id, points: 3)
+
+      expect(user_presented.skip_rate).to eq 50.0
+    end
+
+    it "returns 0 if user havent voted any stories" do
+      expect(user_presented.skip_rate).to eq 0
+    end
+  end
+
+  describe "#avg_per_story" do
+    it "returns average time per story if have groomed stories" do
+      UserStoryPoint.create(user_id: user.id, story_id: 1, points: 1)
+      UserStoryPoint.create(user_id: user.id, story_id: 2, points: 3)
+
+      allow(user_presented).to receive(:time_spent) { 100 }
+      expect(user_presented.avg_per_story).to eq 50.0
+    end
+
+    it "returns 0 if havent groomed any stories" do
+      expect(user_presented.avg_per_story).to eq 0
     end
   end
 end
