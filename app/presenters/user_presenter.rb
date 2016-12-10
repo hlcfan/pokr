e661778cf022f185b54a41d7884076155673d815
Alex S <hlcfan.yan@gmail.com>
2016-12-10 09:52:55 +0800

    Recent stories should be easy get

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -16,14 +16,6 @@ class UserPresenter
     @participated_rooms ||= user.rooms.order("created_at DESC").to_a
   end
 
-  def recent_rooms
-    @recent_rooms ||= begin
-      participated_rooms.take(3).select do |room|
-        room.status == Room::DRAW
-      end
-    end
-  end
-
   def timestamp_for_line_chart
     participated_rooms.reverse.map do |room|
       room.created_at.strftime("%b %d")
@@ -37,8 +29,7 @@ class UserPresenter
   end
 
   def recent_stories
-    room_ids = recent_rooms.map(&:id)
-    Story.where(room_id: room_ids).limit(10)
+    Story.order("updated_at DESC").limit(10)
   end
 
 end
