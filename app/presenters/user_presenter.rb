7173913a808f50dbdec98fda62faf855f28fd012
Alex S <hlcfan.yan@gmail.com>
2016-12-10 10:33:40 +0800

    Use SimpleDelegator instead

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,19 +1,13 @@
-class UserPresenter
-
-  attr_accessor :user
-
-  def initialize user
-    @user = user
-  end
+class UserPresenter < SimpleDelegator
 
   def created_rooms
     participated_rooms.select do |room|
-      user.id == room.created_by
+      id == room.created_by
     end
   end
 
   def participated_rooms
-    @participated_rooms ||= user.rooms.order("created_at DESC").to_a
+    @participated_rooms ||= rooms.order("created_at DESC").to_a
   end
 
   def timestamp_for_line_chart
