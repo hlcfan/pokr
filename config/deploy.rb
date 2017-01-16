d3deb522f67eb7ebf03ddadef8092e059b32b0b6
Alex S <hlcfan.yan@gmail.com>
2017-01-16 17:10:06 +0800

    Correct deploy share path

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,7 @@ set :puma_state, 'tmp/sockets/puma.state'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log', 'tmp/pids', 'tmp/sockets']
-set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
+set :shared_paths, ['config/database.yml', 'log', 'tmp/pids', 'tmp/sockets', 'public/system']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
