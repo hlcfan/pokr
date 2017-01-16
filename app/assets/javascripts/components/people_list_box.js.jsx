8a54170f90dc31efb90218bf3787043dbefc901e
Alex S <hlcfan.yan@gmail.com>
2017-01-16 21:35:46 +0800

    Reset state everytime it refreshes

--- app/assets/javascripts/components/people_list_box.js.jsx
+++ app/assets/javascripts/components/people_list_box.js.jsx
@@ -5,6 +5,7 @@ var PeopleListBox = React.createClass({
       dataType: 'json',
       cache: false,
       success: function(data) {
+        this.setState({data: []});
         this.setState({data: data});
         EventEmitter.dispatch("showResultPanel");
         if (callback) {
