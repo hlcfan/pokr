c081b466a700f92b187d8bf691c4a004b041aaa6
Alex S <hlcfan.yan@gmail.com>
2017-01-19 08:40:15 +0800

    Add String extention to check valid url

--- app/assets/javascripts/ext.js
+++ app/assets/javascripts/ext.js
@@ -11,3 +11,8 @@ Array.prototype.clean = function(deleteValue) {
 Array.prototype.remove = function(v) {
   this.splice(this.indexOf(v) == -1 ? this.length : this.indexOf(v), 1);
 }
+
+String.prototype.isValidUrl = function() {
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  return !!regex .test(this)
+}
