ae726027abdef7af8dafdcda0f33ea268081fe1a
Alex S <hlcfan.yan@gmail.com>
2017-02-17 12:53:39 +0800

    Correct page layout

--- app/assets/javascripts/components/room.js.jsx
+++ app/assets/javascripts/components/room.js.jsx
@@ -8,10 +8,8 @@ var Room = React.createClass({
   },
   render: function() {
     return (
-      <div>
-        <div className="col-md-12 name">
-          <StatusBar />
-        </div>
+      <div className="row">
+        <StatusBar />
         <div id="operationArea" className="col-md-8">
           <VoteBox poker={POKER}/>
           <StoryListBox url={this.props.poker.storyListUrl} />
--- app/assets/javascripts/components/status_bar.js.jsx
+++ app/assets/javascripts/components/status_bar.js.jsx
@@ -1,10 +1,18 @@
 var StatusBar = React.createClass({
   render:function(){
     return (
-      <h3>
-        {POKER.roomName}
-        <i className="pull-right">Yo, {POKER.currentUser.name}({POKER.role})!</i>
-      </h3>
+      <div className="name">
+        <div className="col-md-8">
+          <h3 className="pull-left">{POKER.roomName}</h3>
+          <div className="btn-group pull-right" role="group" aria-label="...">
+            <button type="button" className="btn btn-default">Edit</button>
+            <button type="button" className="btn btn-default btn-warning">Close</button>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <h3><i className="pull-right">Yo, {POKER.currentUser.name}({POKER.role})!</i></h3>
+        </div>
+      </div>
     );
   }
 });
