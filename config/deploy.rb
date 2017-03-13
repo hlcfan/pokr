bd0304162fcb40fbd7a02fe5fd1c6751e52162c3
Alex S <hlcfan.yan@gmail.com>
2017-03-13 22:40:11 +0800

    Forget rake tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,6 +92,30 @@ task :seed => :environment do
   queue  %[echo "-----> Rake Seeding Completed."]
 end
 
+namespace :whenever do
+  desc "Clear crontab"
+  task :clear do
+    queue %{
+      echo "-----> Clear crontab for #{domain}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --clear-crontab #{domain} --set 'environment=production&path=#{deploy_to!}/#{current_path!}']}
+    }
+  end
+  desc "Update crontab"
+  task :update do
+    queue %{
+      echo "-----> Update crontab for #{domain}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --update-crontab #{domain} --set 'environment=production&path=#{deploy_to!}/#{current_path!}']}
+    }
+  end
+  desc "Write crontab"
+  task :write do
+    queue %{
+      echo "-----> Update crontab for #{domain}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; bundle exec whenever --write-crontab #{domain} --set 'environment=production&path=#{deploy_to!}/#{current_path!}']}
+    }
+  end
+end
+
 namespace :god do
   desc "Start God"
   task :start => :environment do
