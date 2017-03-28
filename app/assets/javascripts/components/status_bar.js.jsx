e04d595b160cb1d0631c552377d0f0215410f5ad
Alex S <hlcfan.yan@gmail.com>
2017-03-28 13:36:38 +0800

    Add button with share button with tooltip

--- app/assets/javascripts/components/status_bar.js.jsx
+++ app/assets/javascripts/components/status_bar.js.jsx
@@ -12,6 +12,7 @@ var StatusBar = React.createClass({
     $(".room-operation").remove();
   },
   componentDidMount: function() {
+    $('[data-toggle="tooltip"]').tooltip({container: "#tooltip-area"});
     EventEmitter.subscribe("roomClosed", this.removeOperationButtons);
   },
   render:function() {
@@ -20,18 +21,29 @@ var StatusBar = React.createClass({
       var buttonText = "Close it";
       var buttonClassName = "btn-warning close-room";
       var onClickHandler = that.closeRoom;
-      
+
       return (
         <button type="button" onClick={onClickHandler} className={"btn btn-default " + buttonClassName}>{buttonText}</button>
       )
     }();
 
+    var copyLink = function() {
+      var aField = document.getElementById("hiddenField");
+      aField.hidden   = false;
+      aField.value    = window.location.href;
+      aField.select();
+      document.execCommand("copy");
+      // alert("Following text has been copied to the clipboard.\n\n" + aField.value);
+      aField.hidden = true;
+    }
+
     var operationButtons = function() {
       if (POKER.role === 'Moderator' && POKER.roomState !== "draw") {
         return(
           <div className="btn-group pull-right room-operation" role="group">
-            <a href={"/rooms/"+ POKER.roomId + "/edit"} className="btn btn-default">Edit room</a>
             {roomStatusButton}
+            <a href={"/rooms/"+ POKER.roomId + "/edit"} className="btn btn-default">Edit room</a>
+            <button type="button" onClick={copyLink} className="btn btn-default" data-toggle="tooltip" data-placement="bottom" data-trigger="click" title="Click to copy room link">Share link</button>
           </div>
         )
       }
@@ -42,10 +54,12 @@ var StatusBar = React.createClass({
         <div className="col-md-8">
           <h3 className="pull-left">{POKER.roomName}</h3>
           {operationButtons}
+          <div id="tooltip-area"></div>
         </div>
         <div className="col-md-4">
           <h3><i className="pull-right">Yo, {POKER.currentUser.name}({POKER.role})!</i></h3>
         </div>
+        <input type="text" id="hiddenField" className="room--share-link" />
       </div>
     );
   }
