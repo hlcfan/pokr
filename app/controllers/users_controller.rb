cb7eb2bafb05c968616eebc08576f929855e790e
Alex S <hlcfan.yan@gmail.com>
2017-04-01 16:53:54 +0800

    Filter out current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
     head :bad_request and return if query.blank?
 
     users = User.where("name like ? OR email like ?", "#{query}%", "#{query}%").pluck(:id, :name).map do |id, name|
+      next if id == current_user.id
       { id: id, label: name, value: name }
-    end
+    end.compact
 
     render json: users
   end
