41ccf162c7cb0eb97450f2e8226085397ba29b2f
Alex S <hlcfan.yan@gmail.com>
2017-06-19 13:01:58 +0800

    Rename locale file name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] ||
-    extract_locale_from_accept_language_header ||
-    I18n.default_locale
+      extract_locale_from_accept_language_header ||
+      I18n.default_locale
   end
 
   def extract_locale_from_accept_language_header
--- config/locale.rb
+++ config/locale.rb
@@ -2,7 +2,7 @@
 # I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
  
 # Whitelist locales available for the application
-I18n.available_locales = [:en, :zh_cn]
+I18n.available_locales = [:en, "zh-CN"]
  
 # Set default locale to something other than :en
 I18n.default_locale = :en
