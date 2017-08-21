c3966bbec43542f3e64c79d48e7192c7a4e9a625
Alex S <hlcfan.yan@gmail.com>
2017-08-21 20:02:39 +0800

    Add index to user rooms
    There're queries like:
    ```
    SELECT `rooms`.* FROM `rooms` INNER JOIN `user_rooms` ON `rooms`.`id` = `user_rooms`.`room_id` WHERE `user_rooms`.`user_id` = ? ORDER BY created_at DESC LIMIT 10 OFFSET ?
    ```
    For table user_rooms, it only has index index_user_rooms_on_user_id_and_room_id
    it wouldn't work for the query above, I have to create one index index_user_rooms_on_room_id
    to make it utilize the index instead of scanning table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728030255) do
+ActiveRecord::Schema.define(version: 20170821120103) do
 
   create_table "authorizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170728030255) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+    t.index ["room_id"], name: "index_user_rooms_on_room_id"
     t.index ["user_id", "room_id"], name: "index_user_rooms_on_user_id_and_room_id", unique: true
   end
 
