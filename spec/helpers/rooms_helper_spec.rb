341114a8b0c60076533821399ddf0d1aa1448953
Alex S <hlcfan.yan@gmail.com>
2017-09-24 09:21:19 +0800

    Add tests

--- spec/helpers/rooms_helper_spec.rb
+++ spec/helpers/rooms_helper_spec.rb
@@ -24,5 +24,16 @@ RSpec.describe RoomsHelper, type: :helper do
       expect(html).to include '<input class="btn btn-info" type="button" value="13" />'
       expect(html).to include '<input class="btn btn-default" type="button" value="20" />'
     end
+
+    it "generates form input tag with enabled button style for point values of rooms" do
+      html = render_point_values true, %w(0 1 2 5 8 13 20), %w(1 5 13)
+      expect(html).to include '<input class="btn btn-info" type="button" value="0" />'
+      expect(html).to include '<input class="btn btn-info" type="button" value="1" />'
+      expect(html).to include '<input class="btn btn-info" type="button" value="2" />'
+      expect(html).to include '<input class="btn btn-info" type="button" value="5" />'
+      expect(html).to include '<input class="btn btn-info" type="button" value="8" />'
+      expect(html).to include '<input class="btn btn-info" type="button" value="13" />'
+      expect(html).to include '<input class="btn btn-info" type="button" value="20" />'
+    end
   end
 end
