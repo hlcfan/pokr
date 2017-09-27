c08e8df815918ce4f3789ef848ec4db04e69a9ca
Alex S <hlcfan.yan@gmail.com>
2017-09-27 22:13:10 +0800

    Disable timeme for ie 10 and 11

---
+++ client/app/libs/helper.js
@@ -0,0 +1,15 @@
+export default {
+  ieBrowser11() {
+    let trident = {
+      string: navigator.userAgent.match(/Trident\/(\d+)/)
+    }
+
+    trident.version = trident.string ? parseInt(trident.string[1], 10) : null;
+    alert(trident.version)
+    if (trident.version === 7 || trident.version === 6) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
