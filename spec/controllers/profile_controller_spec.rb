82674d3c921bf8799f173fc0d4639170bf3f2897
Alex S <hlcfan.yan@gmail.com>
2018-01-13 21:53:21 +0800

    Add tests for update and update_password

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ProfileController, type: :controller do
-  
+
   describe "GET #show" do
     context "when user logged in" do
       login_user
@@ -37,6 +37,14 @@ RSpec.describe ProfileController, type: :controller do
         expect(User.last.name).to eq "alex"
         expect(response).to render_template "show"
       end
+
+      it "allows to update email and removes guest user identifier if guest user" do
+        @request.session[:guest_user_id] = "what-ever-id"
+        patch :update, params: { user: { name: 'name-to-be', email: 'b@b.com', avatar_file_name: 'avatar.png', avatar_content_type: 'image/jpeg' } }
+        expect(User.last.email).to eq "b@b.com"
+        expect(response).to redirect_to profile_path
+        expect(session[:guest_user_id]).to be_nil
+      end
     end
 
     context "when user does not logged in" do
@@ -65,6 +73,13 @@ RSpec.describe ProfileController, type: :controller do
         allow_any_instance_of(User).to receive(:update) { false }
         patch :update_password, params: { user: { current_password: 'password', password: 'password-update', 'password_confirmation': 'password-update' } }
         expect(response).to render_template "show"
+      end
+
+      it "updates user password without verifying current password if guest user" do
+        @request.session[:guest_user_id] = "what-ever-id"
+        patch :update_password, params: { user: { password: 'password-update', 'password_confirmation': 'password-update' } }
+        expect(User.last.valid_password?("password-update")).to be true
+        expect(response).to redirect_to profile_path
       end
     end
 
