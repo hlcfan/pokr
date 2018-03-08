7a20d9d039751afe23ed5fc0467592459ce3f35a
Alex S <hlcfan.yan@gmail.com>
2018-03-08 22:13:49 +0800

    Tune it

--- config/initializers/pg_search.rb
+++ config/initializers/pg_search.rb
@@ -2,7 +2,7 @@ PgSearch.multisearch_options = {
   :using => {
     :tsearch => { :dictionary  => 'english'},
     :trigram => {
-      :threshold => 0.5
+      :threshold => 0.4
     }
   }
   # :ignoring => :accents
