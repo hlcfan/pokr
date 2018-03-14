27dbaa116dac2c76fcf355ad0e56fda61b176ab3
Alex S <hlcfan.yan@gmail.com>
2018-03-14 21:33:22 +0800

    Add related room/story check and authentication

--- app/controllers/typeahead_controller.rb
+++ app/controllers/typeahead_controller.rb
@@ -1,12 +1,17 @@
 class TypeaheadController < ApplicationController
   include StoryHelper
 
+  before_action :authenticate_user!
+
   def index
     query = params[:query]
     head :bad_request and return if query.blank?
     searches = PgSearch.multisearch(query).limit(10)
     searches = searches.map do |search_object|
-      search_object.searchable
+      story_or_room = search_object.searchable
+      if story_or_room.related_to_user? current_user.id
+        search_object.searchable
+      end
     end.group_by { |search| search.class }
 
     render json: format_typeahead_data(searches)
--- spec/controllers/typeahead_controller_spec.rb
+++ spec/controllers/typeahead_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe TypeaheadController, type: :controller do
 
   describe "#index" do
+    login_user
+
     it "returns 400 if no query provided" do
       get :index
       expect(response.status).to eq 400
@@ -10,7 +12,10 @@ RSpec.describe TypeaheadController, type: :controller do
 
     it "returns grouped typeahead data" do
       room = Room.create(name: "room name")
-      Story.create(link: "related room story", desc: "description", room_id: room.id, point: "13")
+      user = User.find_by(email: "a@a.com")
+      story = Story.create(link: "related room story", desc: "description", room_id: room.id, point: "13")
+      UserRoom.create(user_id: user.id, room_id: room.id)
+      UserStoryPoint.create(user_id: user.id, story_id: story.id, points: "13")
       PgSearch::Multisearch.rebuild(Room)
       PgSearch::Multisearch.rebuild(Story)
       get :index, format: :json, params: {query: "room"}
