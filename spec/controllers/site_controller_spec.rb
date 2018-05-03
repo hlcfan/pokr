8ae76636c43a9c0f54aec52e50a39f2f3e4422eb
Alex S <hlcfan.yan@gmail.com>
2018-05-03 22:09:44 +0800

    Add test

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe SiteController, type: :controller do
-  
+
   describe "GET #about" do
     it "renders about page" do
       get :about
       expect(response).to render_template "about.en"
     end
+  end
 
+  describe "GET #faq" do
     it "renders faq page" do
       get :faq
       expect(response).to render_template "faq.en"
+    end
+  end
+
+  describe "GET #donate" do
+    it "renders donate page" do
+      get :donate
+      expect(assigns[:donations]).to be_kind_of(Array)
+      expect(response).to render_template "donate.en"
     end
   end
 end
