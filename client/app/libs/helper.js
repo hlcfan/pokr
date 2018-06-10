dd49488eb4a2e0eb065f75490cef6860b87f7e8b
Alex S <hlcfan.yan@gmail.com>
2018-06-10 18:07:09 +0800

    Add helper functions to transform Jira urls

--- client/app/libs/helper.js
+++ client/app/libs/helper.js
@@ -10,5 +10,21 @@ export default {
     } else {
       return false
     }
+  },
+
+  jiraTicketUrlForApi(clientUrl) {
+    // http://localhost:8080/rest/api/2/issue/POK-1
+    // http://localhost:8080/browse/POK-4
+    const anchorTag = document.createElement('a')
+    anchorTag.href = clientUrl
+    const [, ticketId] = /\/browse\/(.*)/.exec(anchorTag.pathname)
+    return `${anchorTag.origin}/rest/api/2/issue/${ticketId}`
+  },
+
+  jiraTicketUrlForClient(apiUrl) {
+    const anchorTag = document.createElement("a")
+    anchorTag.href = apiUrl
+    const [, ticketId] = /\/rest\/api\/2\/issue\/(.*)/.exec(anchorTag.pathname)
+    return `${anchorTag.origin}/browse/${ticketId}`
   }
 }
