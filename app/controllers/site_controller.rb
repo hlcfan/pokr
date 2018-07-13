b3351f13553ff66230f5a907b0222038d096d0a3
Alex S <hlcfan.yan@gmail.com>
2018-07-13 21:28:48 +0800

    Matt++

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -12,9 +12,10 @@ class SiteController < ApplicationController
 
   def donate
     @donations = [
-      { name: "Noah",  amount: MONEY_BAG*40 },
-      { name: "Renee", amount: MONEY_BAG*5 },
-      { name: "Kevin", amount: MONEY_BAG*8 }
+      { name: "Matthew", amount: MONEY_BAG*66 },
+      { name: "Noah",    amount: MONEY_BAG*40 },
+      { name: "Renee",   amount: MONEY_BAG*5 },
+      { name: "Kevin",   amount: MONEY_BAG*8 }
     ]
 
     render "donate.#{I18n.locale}"
