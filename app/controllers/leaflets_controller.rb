1764a2b09e9c695cf0fcc77b97d50a1e3edcf5e7
Alex S <hlcfan.yan@gmail.com>
2018-08-21 21:54:16 +0800

    Can show leaflet now

--- app/controllers/leaflets_controller.rb
+++ app/controllers/leaflets_controller.rb
@@ -11,7 +11,56 @@ class LeafletsController < ApplicationController
     @leaflet.stories.build
   end
 
+  def create
+    @leaflet = repo.new_entity(leaflet_params.merge(created_by: current_user.id))
+    respond_to do |format|
+      if repo.save @leaflet
+        format.html { redirect_to leaflet_path(@leaflet.slug), notice: "Leaflet was successfully created." }
+        format.json { render :show, status: :created, location: @leaflet }
+      else
+        format.html { render :new }
+        format.json { render json: @leaflet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
 
+  end
+
+  private
+
+  def enter_room
+    user_room = UserRoom.find_or_initialize_by(user_id: current_user.id, room_id: @room.id)
+    if user_room.new_record?
+      user_room.update!(role: UserRoom::PARTICIPANT)
+      broadcaster "rooms/#{@room.slug}",
+        user_id: current_user.id,
+        data: 'refresh-users',
+        type: 'action'
+    end
+  end
+
+
+  def set_room
+    @room = Room.find_by(slug: params[:id])
+    raise ActiveRecord::RecordNotFound if @room.nil?
+  end
+
+  def guest_check
+    if current_user.nil?
+      redirect_to screen_room_path(params[:id])
+    end
+  end
+
+  def repo
+    @repo ||= RoomRepository.new
+  end
+
+  def leaflet_params
+    params.require(:room).permit(
+      :name, :pv, :bulk, :bulk_links, :scheme,
+      stories_attributes: [:id, :link, :desc, :_destroy]
+    )
   end
 end
