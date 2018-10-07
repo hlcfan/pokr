30b52a606b66827f461e9db39d7616bcc872cb14
Alex S <hlcfan.yan@gmail.com>
2018-10-07 11:20:04 +0800

    Wut

--- app/mailers/manual_mailer.rb
+++ app/mailers/manual_mailer.rb
@@ -1,6 +1,6 @@
 class ManualMailer < ApplicationMailer
 
-  default from: "hlcfan.yan@gmail.com" # , "Message-ID" => -> {"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@pokrex.com>"}
+  default from: "hlcfan.yan@gmail.com", "Message-ID" => -> {"<#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@pokrex.com>"}
   layout 'mailer'
 
   def create to:, message:
