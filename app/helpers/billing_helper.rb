6516bddc6709e57b357c80edc995e77abbeb19cc
Alex S <hlcfan.yan@gmail.com>
2018-10-15 19:50:34 +0800

    Fix logic

--- app/helpers/billing_helper.rb
+++ app/helpers/billing_helper.rb
@@ -1,5 +1,5 @@
 module BillingHelper
   def allow_to_create_schems?
-    current_user.premium? || (!current_user.premium? && current_user.schemes.count <= 1)
+    current_user.premium? || (!current_user.premium? && current_user.schemes.count < 1)
   end
 end
