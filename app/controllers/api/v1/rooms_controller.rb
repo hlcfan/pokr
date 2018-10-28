d40a1810ae9beb618f0a6d50e4d889147c17a7b9
Alex S <hlcfan.yan@gmail.com>
2018-10-28 22:53:59 +0800

    Return tickets info

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -4,9 +4,20 @@ module Api
       include ActionView::Rendering
 
       before_action :authenticate_user!
+      before_action :set_room, only: [:show]
 
       def index
         @rooms = UserPresenter.new(current_user).participated_rooms
+      end
+
+      def show
+      end
+
+      private
+
+      def set_room
+        @room = Room.find_by(slug: params[:id])
+        raise ActiveRecord::RecordNotFound if @room.nil?
       end
     end
   end
--- app/views/api/v1/rooms/index.json.jbuilder
+++ app/views/api/v1/rooms/index.json.jbuilder
@@ -1,9 +1,9 @@
 json.success "true"
 json.data do
   json.array! @rooms do |room|
-    json.name room.name
     json.id room.slug
+    json.name room.name
     json.create_date l(room.created_at, format: :long)
-    json.url room_url(room.slug)
+    json.link room_url(room.slug)
   end
 end
