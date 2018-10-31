8bc3d273648357a58c7617172055971d48a3a336
Alex S <hlcfan.yan@gmail.com>
2018-10-31 15:25:42 +0800

    Return all schemes

--- app/controllers/api/v1/schemes_controller.rb
+++ app/controllers/api/v1/schemes_controller.rb
@@ -6,7 +6,18 @@ module Api
       before_action :authenticate_user!
 
       def index
-        @schemes = Scheme.schemes_of(current_user.id)
+        @custom_schemes = Scheme.where( user_id: current_user.id ).inject({}) do|hash, scheme|
+          hash[scheme.slug] = scheme.name
+          hash
+        end
+
+        # @custom_schemes.merge!(Scheme.default_schemes)
+        default_schemes = Scheme.default_schemes.inject({}) do |hash, (k,v)|
+          hash[k] = k.parameterize
+          hash
+        end
+
+        @custom_schemes.merge! default_schemes
       end
     end
   end
--- app/views/api/v1/schemes/index.json.jbuilder
+++ app/views/api/v1/schemes/index.json.jbuilder
@@ -1,7 +1,2 @@
 json.success "true"
-json.data do
-  json.array! @schemes do |scheme|
-    json.name scheme.name
-    json.id scheme.slug
-  end
-end
+json.data @custom_schemes
