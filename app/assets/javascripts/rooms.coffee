hlcfan/pokr

commit 3a97c4c54125367932cd2bceeb756b0a1cc6eaf7

Fix autocomplete while creating room

app/assets/javascripts/rooms.coffee
@@ -76,11 +76,11 @@ class Rooms
     $('#room_moderator_ids').tagsinput
       typeaheadjs:
         name: 'matchedUsers'
-        displayKey: 'name'
+        displayKey: 'value'
 #        valueKey: 'name'
         source: matchedUsers.ttAdapter()
-      itemValue: 'value'
-      itemText: 'name'
+      itemValue: 'id'
+      itemText: 'value'
       allowDuplicates: false
       freeInput: false
 

commit b46b134688b51252a794f257104025bd2b4760c3

Fix bug introduced by https://github.com/hlcfan/pokr/commit/3a97c4c54125367932cd2bceeb756b0a1cc6eaf7

app/assets/javascripts/rooms.coffee
@@ -101,7 +101,7 @@ class Rooms
     $roomModerators = $("#room-moderators")
     roomModerators = JSON.parse($roomModerators.val())
     $.each roomModerators, (index, user) ->
-      $('#room_moderator_ids').tagsinput('add', { value: user.value, name: user.name })
+      $('#room_moderator_ids').tagsinput('add', { "id": user.value, "value": user.name })
 
     el = document.getElementById('story-row')
     sortable = Sortable.create(el, {