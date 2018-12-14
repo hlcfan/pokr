acbda83a13dffe0e89b0ca574de8d0ac948a4813
Alex S <hlcfan.yan@gmail.com>
2018-12-14 16:40:43 +0800

    Update text and image to entice users to use browser extension

--- client/app/bundles/Room/components/Synk/Guide.jsx
+++ client/app/bundles/Room/components/Synk/Guide.jsx
@@ -12,16 +12,19 @@ export default class Guide extends React.Component {
             <div className="modal-content">
               <div className="modal-header">
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 className="modal-title">Instruction to sync to JIRA</h4>
+                <h4 className="modal-title">Install Chrome extension - Pokrex JIRA Assistant</h4>
               </div>
               <div className="modal-body">
-                <h3 className={`${css["installation-link"]} text-center`}>
-                  <a href="https://pokrex.com/apps?from_room" target="_blank">Install app and start sync within 2 minutes</a>
-                </h3>
+                <ol>
+                  <li><a href="/pokrex-chrome-extention-pack.zip"><b>Download</b></a> and extract extension zip file (95kb)</li>
+                  <li>Open URL: <b>chrome://extensions/</b> in Chrome</li>
+                  <li><b>Drag</b> extracted extension folder into the page</li>
+                </ol>
+                <img className={css["installation-screenshot"]} src="/installachromeextension.png" alt="drag and install extension"/>
               </div>
               <div className="panel-footer">
                 <div className="alert alert-success" role="alert">
-                  <p><i className="fa fa-question-circle"></i> Because the <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS" target="_blank">CORS policy</a>, Pokrex cannot send/receive data from JIRA, thus we have to create desktop applications to send/receive data to JIRA from your computer, just like what you do in browser. It's totally secure, we do not store any of your credentials, all requests are made locally.</p>
+                  <p><i className="fa fa-question-circle"></i> Because the <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS" target="_blank">CORS policy</a>, Pokrex cannot send/receive data from JIRA, thus we have to create browser extension to send/receive data to JIRA from your browser. It's totally secure, we do not store any of your credentials, all requests are made locally.</p>
                 </div>
               </div>
             </div>
