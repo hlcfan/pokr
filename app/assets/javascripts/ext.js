cfef5dc980cad310f84e23963e7fc6496f0875d3
Alex S <hlcfan.yan@gmail.com>
2019-01-28 19:03:35 +0800

    Remove useless code

--- app/assets/javascripts/ext.js
+++ app/assets/javascripts/ext.js
@@ -1,6 +1,6 @@
 Array.prototype.clean = function(deleteValue) {
   for (var i = 0; i < this.length; i++) {
-    if (this[i] === deleteValue) {         
+    if (this[i] === deleteValue) {
       this.splice(i, 1);
       i--;
     }
