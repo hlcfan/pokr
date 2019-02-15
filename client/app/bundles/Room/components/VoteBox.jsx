790f149e4a4a5f1865089f56fd5d05a02bd08b82
Alex S <hlcfan.yan@gmail.com>
2019-02-15 18:46:20 +0800

    Make it way more straight forward

--- client/app/bundles/Room/components/VoteBox.jsx
+++ client/app/bundles/Room/components/VoteBox.jsx
@@ -51,7 +51,10 @@ export default class VoteBox extends React.Component {
 
     return (
       <div className="panel panel-default" id="deck">
-        <div className="panel-heading">Vote</div>
+        <div className="panel-heading">
+          Vote
+          <span className="pull-right"><i className="fa fa-info-circle"></i> Click to vote</span>
+        </div>
         <div className="vote-list panel-body">
           <ul className="list-inline">
             {pointsList}
