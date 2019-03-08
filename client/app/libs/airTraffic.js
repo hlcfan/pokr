19a69ab6be273db3f53250c3bd43422218507926
Alex S <hlcfan.yan@gmail.com>
2019-03-08 20:11:03 +0800

    Prime for message bus

--- client/app/libs/airTraffic.js
+++ client/app/libs/airTraffic.js
@@ -7,49 +7,64 @@ window.App = {}
 
 export default {
   setupChannelSubscription(roomId) {
-    App.cable = ActionCable.createConsumer()
+    MessageBus.start()
+    MessageBus.callbackInterval = 500
 
-    App.rooms = App.cable.subscriptions.create({channel: 'RoomsChannel', room: roomId}, {
-      connected: function(){
-      },
-      received: function(data) {
-        // console.dir(data)
-        if (data.type === 'action') {
-          if (data.data === 'open') {
-            RoomActions.showResultSection()
-          } else if (data.data === 'refresh-users') {
-            if ($("#u-" + data.user_id).length <= 0) {
-              EventEmitter.dispatch("refreshUsers")
-            }
-          } else if(data.data === "next-story") {
-            RoomActions.nextStory()
-          } else if(data.data === "revote") {
-            RoomActions.nextStory()
-          } else if(data.data === "close-room") {
-            EventEmitter.dispatch("roomClosed")
-          } else if(data.data === "switch-roles") {
-            EventEmitter.dispatch("refreshUsers")
-          } else if(data.data === "clear-votes") {
-            RoomActions.nextStory()
-          }
-        } else if(data.type === 'notify') {
-          if (window.syncResult) {
+    MessageBus.subscribe(`/cable/${roomId}`, function(data){
+      // console.dir(data)
+      if (data.type === 'action') {
+        if (data.data === 'open') {
+          RoomActions.showResultSection()
+        } else if (data.data === 'refresh-users') {
+          if ($("#u-" + data.user_id).length <= 0) {
             EventEmitter.dispatch("refreshUsers")
           }
-          // Must keep for now
-          var $personElement = $('#u-' + data.person_id)
-          if ($personElement.hasClass('voted')) {
-            $personElement.removeClass("voted")
-          }
-          setTimeout(function(){
-            $personElement.addClass("voted", 100)
-          }, 200)
-        } else if(data.type === "evictUser") {
-          EventEmitter.dispatch("evictUser", data.data.userId)
+        } else if(data.data === "next-story") {
+          RoomActions.nextStory()
+        } else if(data.data === "revote") {
+          RoomActions.nextStory()
+        } else if(data.data === "close-room") {
+          EventEmitter.dispatch("roomClosed")
+        } else if(data.data === "switch-roles") {
           EventEmitter.dispatch("refreshUsers")
-        } else {
+        } else if(data.data === "clear-votes") {
+          RoomActions.nextStory()
         }
+      } else if(data.type === 'notify') {
+        if (window.syncResult) {
+          EventEmitter.dispatch("refreshUsers")
+        }
+        // Must keep for now
+        var $personElement = $('#u-' + data.person_id)
+        if ($personElement.hasClass('voted')) {
+          $personElement.removeClass("voted")
+        }
+        setTimeout(function(){
+          $personElement.addClass("voted", 100)
+        }, 200)
+      } else if(data.type === "evictUser") {
+        EventEmitter.dispatch("evictUser", data.data.userId)
+        EventEmitter.dispatch("refreshUsers")
+      } else {
       }
     })
+
+    MessageBus.publish = function(action, payload) {
+      console.log(`${action}:${payload}`)
+      fetch(`/rooms/${action}`, {
+        body: JSON.stringify(payload),
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      })
+        .then(data => {
+          console.log(data)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
   }
 }
