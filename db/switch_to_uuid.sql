f972c3f8a50ff06ed4d05b5be26079e40b83f044
Alex S <hlcfan.yan@gmail.com>
2021-03-29 20:17:47 +0800

    Update SQL

--- db/switch_to_uuid.sql
+++ db/switch_to_uuid.sql
@@ -1,57 +1,58 @@
 -- Add uuid column
+
 ALTER TABLE authorizations
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE authorizations
-ADD COLUMN user_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN user_uuid uuid;
 
 ALTER TABLE orders
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE orders
-ADD COLUMN user_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN user_uuid uuid;
 
 ALTER TABLE rooms
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE rooms
-ADD COLUMN created_by_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN created_by_uuid uuid;
 
 ALTER TABLE schemes
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE schemes
-ADD COLUMN user_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN user_uuid uuid;
 
 ALTER TABLE stories
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE stories
-ADD COLUMN room_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN room_uuid uuid;
 
 ALTER TABLE subscriptions
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE subscriptions
-ADD COLUMN user_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN user_uuid uuid;
 
 ALTER TABLE user_rooms
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE user_rooms
-ADD COLUMN user_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN user_uuid uuid;
 
 ALTER TABLE user_rooms
-ADD COLUMN room_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN room_uuid uuid;
 
 ALTER TABLE user_story_points
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
 
 ALTER TABLE user_story_points
-ADD COLUMN user_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN user_uuid uuid;
 
 ALTER TABLE user_story_points
-ADD COLUMN story_uuid uuid default gen_random_uuid() not null;
+ADD COLUMN story_uuid uuid;
 
 ALTER TABLE users
 ADD COLUMN uuid uuid default gen_random_uuid() not null;
@@ -63,6 +64,7 @@ ALTER TABLE pg_search_documents
 ADD COLUMN searchable_uuid uuid;
 
 -- Update foreign keys
+
 UPDATE authorizations a
 SET user_uuid = u.uuid
 FROM users u
@@ -95,32 +97,43 @@ WHERE s.user_id = u.id;
 
 UPDATE user_rooms ur
 SET
-user_uuid = u.uuid,
+user_uuid = u.uuid
+FROM users u
+WHERE ur.user_id = u.id;
+
+UPDATE user_rooms ur
+SET
 room_uuid = r.uuid
-FROM users u, rooms r
-WHERE ur.room_id = r.id AND ur.user_id = u.id;
+FROM rooms r
+WHERE ur.room_id = r.id;
 
 UPDATE user_story_points usp
 SET
-user_uuid = u.uuid,
+user_uuid = u.uuid
+FROM users u
+WHERE usp.user_id = u.id;
+
+UPDATE user_story_points usp
+SET
 story_uuid = s.uuid
-FROM users u, stories s
-WHERE usp.story_id = s.id AND usp.user_id = u.id;
+FROM stories s
+WHERE usp.story_id = s.id;
 
 UPDATE pg_search_documents sd
 SET
-uuid = s.id
+uuid = s.uuid
 FROM stories s
 WHERE searchable_type = 'Story' AND s.id = sd.searchable_id;
 
 UPDATE pg_search_documents sd
 SET
-uuid = r.id
+uuid = r.uuid
 FROM rooms r
 WHERE searchable_type = 'Room' AND r.id = sd.searchable_id;
 
 -- Rename id to integer_id
 -- Rename uuid to id
+
 ALTER TABLE authorizations
 RENAME COLUMN id TO integer_id;
 
@@ -133,6 +146,9 @@ RENAME COLUMN user_id TO integer_user_id;
 ALTER TABLE authorizations
 RENAME COLUMN user_uuid TO user_id;
 
+--ALTER TABLE authorizations
+--ALTER COLUMN user_id SET NOT NULL;
+
 ALTER TABLE orders
 RENAME COLUMN id TO integer_id;
 
@@ -144,6 +160,9 @@ RENAME COLUMN user_id TO integer_user_id;
 
 ALTER TABLE orders
 RENAME COLUMN user_uuid TO user_id;
+
+ALTER TABLE orders
+ALTER COLUMN user_id SET NOT NULL;
 
 ALTER TABLE rooms
 RENAME COLUMN id TO integer_id;
@@ -157,6 +176,9 @@ RENAME COLUMN created_by TO integer_created_by;
 ALTER TABLE rooms
 RENAME COLUMN created_by_uuid TO created_by;
 
+--ALTER TABLE rooms
+--ALTER COLUMN created_by SET NOT NULL;
+
 ALTER TABLE schemes
 RENAME COLUMN id TO integer_id;
 
@@ -168,6 +190,9 @@ RENAME COLUMN user_id TO integer_user_id;
 
 ALTER TABLE schemes
 RENAME COLUMN user_uuid TO user_id;
+
+ALTER TABLE schemes
+ALTER COLUMN user_id SET NOT NULL;
 
 ALTER TABLE stories
 RENAME COLUMN id TO integer_id;
@@ -181,6 +206,9 @@ RENAME COLUMN room_id TO integer_room_id;
 ALTER TABLE stories
 RENAME COLUMN room_uuid TO room_id;
 
+ALTER TABLE stories
+ALTER COLUMN room_id SET NOT NULL;
+
 ALTER TABLE subscriptions
 RENAME COLUMN id TO integer_id;
 
@@ -193,11 +221,11 @@ RENAME COLUMN user_id TO integer_user_id;
 ALTER TABLE subscriptions
 RENAME COLUMN user_uuid TO user_id;
 
-ALTER TABLE user_rooms
-RENAME COLUMN id TO integer_id;
+ALTER TABLE subscriptions
+ALTER COLUMN user_id SET NOT NULL;
 
 ALTER TABLE user_rooms
-RENAME COLUMN uuid TO id;
+RENAME COLUMN id TO integer_id;
 
 ALTER TABLE user_rooms
 RENAME COLUMN uuid TO id;
@@ -207,12 +235,18 @@ RENAME COLUMN user_id TO integer_user_id;
 
 ALTER TABLE user_rooms
 RENAME COLUMN user_uuid TO user_id;
+
+--ALTER TABLE user_rooms
+--ALTER COLUMN user_id SET NOT NULL; --TODO: remove records without user_id and room_id
 
 ALTER TABLE user_rooms
 RENAME COLUMN room_id TO integer_room_id;
 
 ALTER TABLE user_rooms
 RENAME COLUMN room_uuid TO room_id;
+
+--ALTER TABLE user_rooms
+--ALTER COLUMN room_id SET NOT NULL; --TODO: remove records without user_id and room_id
 
 ALTER TABLE user_story_points
 RENAME COLUMN id TO integer_id;
@@ -226,11 +260,19 @@ RENAME COLUMN story_id TO integer_story_id;
 ALTER TABLE user_story_points
 RENAME COLUMN story_uuid TO story_id;
 
+--ALTER TABLE user_story_points
+--ALTER COLUMN story_id SET NOT NULL;
+
+--select * from user_story_points where story_id is null;
+
 ALTER TABLE user_story_points
 RENAME COLUMN user_id TO integer_user_id;
 
 ALTER TABLE user_story_points
 RENAME COLUMN user_uuid TO user_id;
+
+--ALTER TABLE user_story_points
+--ALTER COLUMN user_id SET NOT NULL;
 
 ALTER TABLE users
 RENAME COLUMN id TO integer_id;
@@ -251,9 +293,9 @@ ALTER TABLE pg_search_documents
 RENAME COLUMN searchable_uuid TO searchable_id;
 
 -- Update Primary key
+
 ALTER TABLE authorizations DROP COLUMN integer_id;
 ALTER TABLE authorizations ADD PRIMARY KEY (id);
-ALTER TABLE authorizations DROP COLUMN integer_id;
 ALTER TABLE authorizations DROP COLUMN integer_user_id;
 
 ALTER TABLE orders DROP COLUMN integer_id;
@@ -262,14 +304,16 @@ ALTER TABLE orders DROP COLUMN integer_user_id;
 
 ALTER TABLE pg_search_documents DROP COLUMN integer_id;
 ALTER TABLE pg_search_documents ADD PRIMARY KEY (id);
+ALTER TABLE pg_search_documents DROP COLUMN searchable_integer_id;
 
 ALTER TABLE rooms DROP COLUMN integer_id;
 ALTER TABLE rooms ADD PRIMARY KEY (id);
 
-ALTER TABLE rooms DROP COLUMN created_by;
+ALTER TABLE rooms DROP COLUMN integer_created_by;
 
 ALTER TABLE schemes DROP COLUMN integer_id;
 ALTER TABLE schemes ADD PRIMARY KEY (id);
+ALTER TABLE schemes DROP COLUMN integer_user_id;
 
 ALTER TABLE stories DROP COLUMN integer_id;
 ALTER TABLE stories ADD PRIMARY KEY (id);
@@ -297,11 +341,29 @@ ALTER TABLE users DROP COLUMN integer_id;
 ALTER TABLE users ADD PRIMARY KEY (id);
 
 -- Remove existing uid
+
 ALTER TABLE authorizations DROP COLUMN uid;
-ALTER TABLE orders DROP COLUMN uid;
 ALTER TABLE rooms DROP COLUMN uid;
 ALTER TABLE schemes DROP COLUMN uid;
 ALTER TABLE stories DROP COLUMN uid;
 ALTER TABLE user_rooms DROP COLUMN uid;
 ALTER TABLE user_story_points DROP COLUMN uid;
 ALTER TABLE users DROP COLUMN uid;
+
+-- Add index for new columns
+
+CREATE INDEX idx_authorizations_user_id ON authorizations (user_id);
+CREATE INDEX idx_orders_on_user_id ON orders (user_id);
+CREATE INDEX idx_rooms_on_created_by ON rooms (created_by);
+CREATE INDEX idx_schemes_on_user_id ON schemes (user_id);
+CREATE INDEX idx_stories_on_user_id ON stories (room_id);
+CREATE INDEX idx_subscriptions_on_user_id ON subscriptions (user_id);
+CREATE UNIQUE INDEX idx_user_rooms_on_user_id_and_room_id ON user_rooms (user_id, room_id);
+CREATE INDEX idx_user_rooms_on_room_id ON user_rooms (room_id);
+CREATE UNIQUE INDEX idx_user_story_points_on_user_id_and_story_id ON user_story_points (user_id, story_id);
+CREATE INDEX idx_user_story_points_on_story_id ON user_story_points (story_id);
+
+--select * from user_rooms where user_uuid is null;
+--select * from user_rooms where uid='f44nn3k0';
+--select * from user_rooms where room_uuid is null;
+--select * from user_story_points where story_id is null;
